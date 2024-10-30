39c14f427ad5fca7be1aef411961874f5d447393
nana kashiwagi <nana30708@gmail.com>
2024-10-30 23:16:30 +0900

    kvのスライドイン

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
-//hamburger
+// ハンバーガーメニュー
 
-const ham = document.querySelector("js-hamburger");
-const nav = document.querySelector("js-nav");
 
-ham.addEventListener('click', function () { 
-    ham.classList.toggle("is-active");
-    nav.classList.toggle("is-active");
+// const ham = document.querySelector("js-hamburger");
+// const nav = document.querySelector("js-nav");
+
+// ham.addEventListener('click', function () { 
+//     ham.classList.toggle("is-active");
+//     nav.classList.toggle("is-active");
     
   
+//   });
+  // kv部分のスライドイン
+
+  gsap
+  .timeline()
+  .from(".js_copy", {
+    duration: 1.0 ,
+    autoAlpha: 0,
+    y: 50
+
+  })
+  .from(".js_sub-copy", {
+    duration: 0.6 ,
+    autoAlpha: 0,
+    x: -100
   });
