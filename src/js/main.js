b2ae4d077bb7117af4091f74ae4bca509348cb8f
nana kashiwagi <nana30708@gmail.com>
2024-12-14 00:02:19 +0900

    下層product news contactOK

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 const swiper = new Swiper(".js_success-swiper", {
     loop: true,
+    autoplay: {
+        delay: 3000, // スライド間の待ち時間（ミリ秒）
+        disableOnInteraction: false, // ユーザー操作後も自動再生を継続するか
+      },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
